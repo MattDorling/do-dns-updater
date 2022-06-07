417d12eaaad218bb1f1f4e072f915bb22311af80
MattDorling <dorling.mj@gmail.com>
2022-06-07 02:25:25 +0100

    Add some logging statements

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from time import sleep
 import requests
 import config
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 
 def get_public_ip():
@@ -8,6 +11,7 @@ def get_public_ip():
 
 
 def update_dns(new_ip):
+    logging.info("Fetching record ID from DigitalOcean API...")
     response = requests.get(f'https://api.digitalocean.com/v2/domains/{config.DOMAIN_NAME}/records/',
                             headers={
                                 "Authorization": f"Bearer {config.DO_API_TOKEN}"},
@@ -18,6 +22,9 @@ def update_dns(new_ip):
 
     domain_record_id = response.json()['domain_records'][0]['id']
 
+    logging.debug(f"Record ID: {domain_record_id}")
+
+    logging.info("Updating IP through DigitalOcean API")
     response = requests.patch(f'https://api.digitalocean.com/v2/domains/{config.DOMAIN_NAME}/records/{domain_record_id}/',
                               headers={
                                   "Authorization": f"Bearer {config.DO_API_TOKEN}"},
@@ -26,16 +33,19 @@ def update_dns(new_ip):
                                   'type': config.TYPE,
                                   'data': str(new_ip)
                               })
-    print(response)
+    logging.debug(f"Response: {response}")
 
 
 ip = get_public_ip()
+logging.info(f"Initial IP address is {ip}")
+
 update_dns(ip)
 
+logging.info(f"Waiting for the public IP to change...")
 while True:
     new_ip = get_public_ip()
     if new_ip != ip:
         update_dns(new_ip)
+        logging.info(f"Waiting for the public IP to change...")
         ip = new_ip
-    print('My public IP address is: {}'.format(ip))
     sleep(config.PUBLIC_IP_CHECK_INTERVAL)
